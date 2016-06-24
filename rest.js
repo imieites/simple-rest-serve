7f6a9fe0bb468fc252553bd395c902ec7c73187e
Esteban <garciawarneckee@gmail.com>
2016-06-24 11:39:21 -0300

    correct conditional

--- rest.js
+++ rest.js
@@ -202,6 +202,7 @@ function respondNotifications(req,res,next) {
 function respondProduct(req, res, next){
   var id = req.params.id;
 
+
   var product = {
     "productCode":163205,
     "category":"CLI",
@@ -434,7 +435,7 @@ function respondProduct(req, res, next){
     ]
   }
 
-  if(id === 163205){
+  if(id === product.productCode){
     res.send(product);
   }else{
     res.send("There is no product with that id");
